feb5990192be3349e14796fab71371ee089a476d
MattCumbo99 <mattcumbo99@users.noreply.github.com>
2026-05-18 12:28:43 -0500

    Other tests for users and auth

--- src/main/kotlin/com/yellrecords/services/user/UserController.kt
+++ src/main/kotlin/com/yellrecords/services/user/UserController.kt
@@ -1,18 +1,15 @@
 package com.yellrecords.services.user
 
 import com.yellrecords.services.auth.CustomUserDetails
-import com.yellrecords.services.user.dto.RegistrationInfo
 import com.yellrecords.services.user.dto.UpdateEmailRequest
 import com.yellrecords.services.user.dto.UserDto
 import jakarta.annotation.security.RolesAllowed
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -34,16 +31,6 @@ class UserController(
     fun getMyself(
         @AuthenticationPrincipal user: CustomUserDetails,
     ): UserDto = service.getUserById(user.id)
-
-    @PostMapping
-    @RolesAllowed(UserRole.ADMIN)
-    fun createUser(
-        @RequestBody registerInfo: RegistrationInfo,
-    ): ResponseEntity<UserDto> {
-        val saved = service.createUser(registerInfo)
-
-        return ResponseEntity(saved, HttpStatus.CREATED)
-    }
 
     @PatchMapping("/{id}/email")
     @PreAuthorize("isAuthenticated() && #id == authentication.principal.id")
--- src/test/kotlin/com/yellrecords/services/auth/AuthControllerTest.kt
+++ src/test/kotlin/com/yellrecords/services/auth/AuthControllerTest.kt
@@ -1,35 +1,158 @@
 package com.yellrecords.services.auth
 
 import com.yellrecords.services.BaseH2Test
+import com.yellrecords.services.auth.dto.ChangePasswordRequest
 import com.yellrecords.services.auth.dto.LoginRequest
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.springframework.http.HttpMethod.PATCH
 import org.springframework.http.HttpMethod.POST
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 class AuthControllerTest : BaseH2Test() {
     companion object {
         const val BASE_PATH = "/api/auth"
-        const val LOGIN = "$BASE_PATH/login"
+
+        const val ADMIN_CORRECT_PASSWORD = "admin"
     }
 
-    @Test
-    fun `should login user`() {
-        val loginReq = LoginRequest(username = TestUsers.admin.username, rawPassword = "admin")
+    @Nested
+    inner class Login {
+        @Test
+        fun `should login user`() {
+            val loginReq =
+                LoginRequest(
+                    username = TestUsers.admin.username,
+                    rawPassword = ADMIN_CORRECT_PASSWORD,
+                )
 
-        mockRequest(POST, LOGIN, body = loginReq, token = null).andExpect(status().isOk)
+            mockRequest(
+                requestType = POST,
+                path = "$BASE_PATH/login",
+                body = loginReq,
+                token = null,
+            ).andExpect(status().isOk)
+        }
+
+        @Test
+        fun `non-existing user should return 400 bad request`() {
+            val loginReq = LoginRequest(username = "1d9ub3fhu9", rawPassword = "123456")
+
+            mockRequest(
+                requestType = POST,
+                path = "$BASE_PATH/login",
+                body = loginReq,
+                token = null,
+            ).andExpect(status().isBadRequest)
+        }
+
+        @Test
+        fun `invalid password should return 400 bad request`() {
+            val loginReq =
+                LoginRequest(username = TestUsers.admin.username, rawPassword = "1234561427")
+
+            mockRequest(
+                requestType = POST,
+                path = "$BASE_PATH/login",
+                body = loginReq,
+                token = null,
+            ).andExpect(status().isBadRequest)
+        }
     }
 
-    @Test
-    fun `non-existing user should return 400 bad request`() {
-        val loginReq = LoginRequest(username = "1d9ub3fhu9", rawPassword = "123456")
+    @Nested
+    inner class ChangePassword {
+        @Test
+        fun `should return 403 forbidden on mismatched token when changing password`() {
+            val req =
+                ChangePasswordRequest(
+                    rawCurrent = ADMIN_CORRECT_PASSWORD,
+                    rawNew = "someOtherPassword",
+                    rawNew2 = "someOtherPassword",
+                )
 
-        mockRequest(POST, LOGIN, body = loginReq, token = null).andExpect(status().isBadRequest)
-    }
+            mockRequest(
+                requestType = PATCH,
+                path = "$BASE_PATH/user/${TestUsers.admin.id}/change-password",
+                token = null,
+                body = req,
+            ).andExpect(status().isForbidden)
+        }
 
-    @Test
-    fun `invalid password should return 400 bad request`() {
-        val loginReq = LoginRequest(username = TestUsers.admin.username, rawPassword = "1234561427")
+        @Nested
+        inner class InvalidateRequest {
+            @Test
+            fun `should return 400 bad request on incorrect current password`() {
+                val req =
+                    ChangePasswordRequest(
+                        rawCurrent = "incorrect password",
+                        rawNew = "someOtherPassword",
+                        rawNew2 = "someOtherPassword",
+                    )
 
-        mockRequest(POST, LOGIN, body = loginReq, token = null).andExpect(status().isBadRequest)
+                mockRequest(
+                    requestType = PATCH,
+                    path = "$BASE_PATH/user/${TestUsers.admin.id}/change-password",
+                    token = TestTokens.admin,
+                    body = req,
+                ).andExpect(status().isBadRequest)
+            }
+
+            @Test
+            fun `should return 400 bad request on non-matching new password`() {
+                val req =
+                    ChangePasswordRequest(
+                        rawCurrent = ADMIN_CORRECT_PASSWORD,
+                        rawNew = "someOtherPassword",
+                        rawNew2 = "someOtherpassword",
+                    )
+
+                mockRequest(
+                    requestType = PATCH,
+                    path = "$BASE_PATH/user/${TestUsers.admin.id}/change-password",
+                    token = TestTokens.admin,
+                    body = req,
+                ).andExpect(status().isBadRequest)
+            }
+
+            @Test
+            fun `should return 400 bad request when new password matches current password`() {
+                val req =
+                    ChangePasswordRequest(
+                        rawCurrent = ADMIN_CORRECT_PASSWORD,
+                        rawNew = ADMIN_CORRECT_PASSWORD,
+                        rawNew2 = ADMIN_CORRECT_PASSWORD,
+                    )
+
+                mockRequest(
+                    requestType = PATCH,
+                    path = "$BASE_PATH/user/${TestUsers.admin.id}/change-password",
+                    token = TestTokens.admin,
+                    body = req,
+                ).andExpect(status().isBadRequest)
+            }
+        }
+
+        @Test
+        fun `should change password`() {
+            val req =
+                ChangePasswordRequest(
+                    rawCurrent = ADMIN_CORRECT_PASSWORD,
+                    rawNew = "someOtherPassword",
+                    rawNew2 = "someOtherPassword",
+                )
+
+            mockRequest(
+                requestType = PATCH,
+                path = "$BASE_PATH/user/${TestUsers.admin.id}/change-password",
+                token = TestTokens.admin,
+                body = req,
+            ).andExpect(status().isOk)
+
+            val admin = userRepository.findById(TestUsers.admin.id!!).get()
+            val passwordMatches = passwordEncoder.matches(req.rawNew, admin.passwordHash)
+            passwordMatches shouldBe true
+        }
     }
 }
